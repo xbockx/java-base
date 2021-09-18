34e5407f947577ae8a822791e82beca2ac38030e
lin <imlinbo@live.com>
2021-09-18 18:35:41 +0800

    add springboot2

---
+++ springboot/src/main/java/com/linbo/boot/MainApplication.java
@@ -0,0 +1,14 @@
+package com.linbo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 这是一个Spring Boot应用
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ springboot/src/main/java/com/linbo/boot/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.linbo.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String handle01() {
+        return "Hello, Spring Boot 2!";
+    }
+}
