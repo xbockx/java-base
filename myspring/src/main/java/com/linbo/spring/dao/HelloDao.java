70669a5b8661c56c7d8175c9b17da7c3726f654e
linbo <imlinbo@live.com>
2021-12-15 16:27:34 +0800

    add myspring

---
+++ myspring/src/main/java/com/linbo/spring/dao/HelloDao.java
@@ -0,0 +1,14 @@
+package com.linbo.spring.dao;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author xbockx
+ * @Date 12/15/2021
+ */
+public interface HelloDao {
+
+    List<String> findAll();
+
+}
